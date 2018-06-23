2f842e3b5bb1f116b51161e5fab31baf5a8e2b7b
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 00:33:01 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
+
 def unsafe?(speed)
-
-end
-
-
+  if speed<40||speed>60
+    return true
+  else 
+    return false 
+  end
+end 
 
 def not_safe?(speed)
-	
+  if speed<40 || speed>60 ? return true : return false 
 end
-	
-
-
